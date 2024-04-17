a087cdbb80fb45fff98e695c420f6c0e0047b5e2
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-17 08:59:48 +0100

    counting character in a string

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let occurrences=0;
+  for (let i = 0; i < str.length; i++) {
+    if (char.toUpperCase()==str[i].toUpperCase()) {
+      occurrences++;
+    }
+  }
+  return occurrences;
+}
 
 module.exports = countOccurrences;
