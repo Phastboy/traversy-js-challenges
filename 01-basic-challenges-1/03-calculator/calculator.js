11f9e7e9013e82161d17f02307620d82b9953e39
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-17 03:38:06 +0100

    calculation based on different operator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(a, b, operator) {
+  switch (operator) {
+    case '+':
+      result=a+b;
+      break;
+    case '-':
+      result=a-b;
+      break;
+    case '*':
+      result=a*b;
+      break;
+    case '/':
+      result=a/b;
+      break;
+    default:
+      throw new Error('Invalid operator');
+      break;
+  }
+  return result;
+}
 
 module.exports = calculator;
