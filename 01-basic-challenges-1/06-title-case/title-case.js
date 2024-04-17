c29cfb36bd6775b5434b269c6b4daecd4669e70c
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-17 21:07:15 +0100

    convert a string to a title

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,24 @@
-function titleCase() {}
+function titleCase(str) {
+  let titleStr='';
+  let array=str.split(' ');
+  let char;
+  let newStr;
+  let newArray=[];
+  for (let i in array) {
+    let element = array[i];
+    char=element.split('');
+    char[0]=char[0].toUpperCase();
+    for (let i = 0; i < char.length; i++) {
+      if (i>0) {
+        char[i]=char[i].toLowerCase();
+      }
+    }
+    newStr=char.join('');
+    newArray.push(newStr);
+    
+  }
+  titleStr=newArray.join(' ');
+  return titleStr;
+}
 
 module.exports = titleCase;
