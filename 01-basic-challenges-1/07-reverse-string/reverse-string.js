20426caed8f94d9636d7fb99db265308067cdb41
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-18 11:18:10 +0100

    reversing a string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(str) {
+  let array=[];
+  let reverseStr;
+  let len=str.length-1;
+  for (let i = 0; i < str.length; i++) {
+    char=str[len--];
+    array.push(char);
+  }
+  reverseStr=array.join('');
+  return reverseStr;
+}
 
 module.exports = reverseString;
