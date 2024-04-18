2ef1df90c6f1ec6e3c8d039a54b711e677131ae2
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-18 23:17:27 +0100

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,8 @@
-function isPalindrome() {}
+const reverseStr=require('../07-reverse-string/reverse-string');
+function isPalindrome(str) {
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversedStr = reverseStr(formattedStr);
+  return formattedStr === reversedStr;
+}
 
 module.exports = isPalindrome;
