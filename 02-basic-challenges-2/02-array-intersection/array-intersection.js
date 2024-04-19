58a9598e9b095348cc46c4c540496d0bf24c4a13
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-19 23:58:02 +0100

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(numArrayOne, numArrayTwo) {
+  let arrayIntersection=[];
+  for (let i = 0; i < numArrayOne.length; i++) {
+    const arrayOneElements = numArrayOne[i];
+    for (let i = 0; i < numArrayTwo.length; i++) {
+      const arrayTwolements = numArrayTwo[i];
+      if (arrayOneElements==arrayTwolements) {
+        arrayIntersection.push(arrayOneElements);
+      }
+    }
+  }
+  return arrayIntersection;
+}
 
 module.exports = arrayIntersection;
