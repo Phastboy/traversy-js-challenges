2542e392ce156c6519061d0502a47a006886ca29
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-19 23:49:41 +0100

    FizzBuzzArray

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
-
+function fizzBuzzArray(number) {
+  let array=[];
+  let num=0;
+  for (let i = 0; i < number; i++) {
+    num++;
+    if (num%3==0 && num%5==0) {
+      array.push('FizzBuzz');
+    } else if(num%5==0) {
+      array.push('Buzz');
+    } else if (num%3==0) {
+      array.push('Fizz');
+    } else {
+      array.push(num);
+    }
+  }
+  return array;
+}
+console.log(fizzBuzzArray(45));
 module.exports = fizzBuzzArray;
