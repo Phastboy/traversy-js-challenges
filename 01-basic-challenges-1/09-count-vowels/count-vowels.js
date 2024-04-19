dc723a61d0cdc5e3429574c6536f15ebcdba073a
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-19 13:36:44 +0100

    cout-vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(string) {
+  let str=string.toLowerCase();
+  let count=0;
+  // let char='a' || 'e' || 'i' || 'o' || 'u';
+  for (let i = 0; i < str.length; i++) {
+    strChars=str[i];
+    if (strChars==='a'||
+      strChars==='e'||
+      strChars==='i'||
+      strChars==='o'||
+      strChars==='u'
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
