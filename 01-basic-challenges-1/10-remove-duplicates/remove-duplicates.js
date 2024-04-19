20ac5e17a64fff8eb7c16b0609311f5be645106e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-19 21:55:34 +0100

    remove duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,15 @@
-function removeDuplicates() {}
+function removeDuplicates(array) {
+  let noDuplicates=[];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]!=array[i+1] && i<array.length) {
+      if (!noDuplicates.includes(array[i])) {
+        noDuplicates.push(array[i]);
+      }
+    } else if(array[i]===array[i+1] && i<array.length) {
+    }
+  }
+  return noDuplicates;
+}
+console.log(removeDuplicates([1, 2, 3, 1, 4, 5, 4]));
 
 module.exports = removeDuplicates;
