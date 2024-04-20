c57c6f1587a1001811a6ef5b88d8eb359b2b827c
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-20 12:49:49 +0100

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(numArray) {
+  let number;
+  let n=numArray.length+1;
+  let total=0;
+  let length=numArray.length;
+  if (length===0) {
+    number=1;
+  } else {
+    for (let i = 0; i < numArray.length; i++) {
+      const element = numArray[i];
+      total+=element;
+    }
+    let assumedTotal=(n*(n+1))/2;
+    number=assumedTotal-total;
+  }
+  return number;
+}
 
 module.exports = findMissingNumber;
