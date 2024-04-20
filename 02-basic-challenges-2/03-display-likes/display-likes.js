f902aa7bf304f8037a549e5dcf13a7cf68e0af73
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-20 11:25:49 +0100

    display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,16 @@
-function displayLikes() {}
+function displayLikes(strArray) {
+  let length=strArray.length
+  if (strArray=='') {
+    return ('no one likes this');
+  } else if (length===1){
+    return `${strArray[0]} likes this`;
+  }else if(length===2) {
+    return `${strArray[0]} and ${strArray[1]} like this`;
+  }else if(length===3) {
+    return `${strArray[0]}, ${strArray[1]} and ${strArray[2]} like this`;
+  }else {
+    return `${strArray[0]}, ${strArray[1]} and ${length-2} others like this`
+  }
+}
 
 module.exports = displayLikes;
