a53d0b76f147f23989d4c0b221b49ba318b32859
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-20 20:29:14 +0100

    are all characters unique?

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,14 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  let charArray=[];
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    if (charArray.includes(element)) {
+      return false;
+    } else {
+      charArray.push(element);
+    }
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
