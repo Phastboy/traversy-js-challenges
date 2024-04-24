b8027e3ed01d05f9b4ea6f8d81fa6514822e515c
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-24 22:59:41 +0100

    format phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbersArray) {
+  let areaCode=numbersArray.slice(0, 3).join('');
+  let prefix=numbersArray.slice(3, 6).join('');
+  let lineNumber=numbersArray.slice(6).join('');
+  let phoneNumber=`(${areaCode}) ${prefix}-${lineNumber}`;
+  return phoneNumber;
+}
 
 module.exports = formatPhoneNumber;
