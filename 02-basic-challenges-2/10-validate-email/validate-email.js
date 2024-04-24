f3d3850f79fe8d2ccfe77ab149bc4772e2a58cb0
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-24 22:58:30 +0100

    validate email

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,22 @@
-function validateEmail() {}
+function validateEmail(email) {
+  if (email.indexOf('@')===-1) {
+    return false;
+  }
+
+  const [localPart, domain]=email.split('@');
+  if (localPart.length===0 || domain<3) {
+    return false;
+  }
+  const domainExtention=domain.split('.');
+  console.log(domainExtention);
+  
+  if (domainExtention.length<2 ||
+  domainExtention[domainExtention.length-1].length<2) {
+    return false;
+  }
+
+  return true;
+}
+console.log(validateEmail('jane.doe@domain.org'));
 
 module.exports = validateEmail;
