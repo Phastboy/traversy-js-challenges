29fa0c137c62d1dc6bd87fef06a2911f872a1d51
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2024-04-25 12:09:42 +0100

    sum of even numbers

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,15 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNumbers=numbers.filter((item) => {
+    return item%2===0;
+  });
+  const squares=evenNumbers.map((item) => {
+    return item*item;
+  });
+  const sumOfEvenSquares=squares.reduce((previous, current) => {
+    return previous+current;
+  }, 0);
+  return sumOfEvenSquares;
+}
+console.log(sumOfEvenSquares([1, 2, 3, 4, 5]));
 
 module.exports = sumOfEvenSquares;
